bc2efec311d8dbbf05705c25f2e4f3a267823368
Alexander Kerckhofs <alexander.kerckhofs@mycsn.be>
2020-08-18 14:39:57 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TicketReaderTcpConn {
+    class Program {
+        static void Main(string[] args) {
+            using var client = new TcpClient();
+            client.Connect("127.0.0.1", 2005);
+
+            using var stream = client.GetStream();
+
+            while (true) {
+                if (stream.CanRead) {
+                    var buffer = new byte[64];
+                    var barcode = new StringBuilder();
+
+                    // Incoming message may be larger than the buffer size.
+                    do {
+                        var read = stream.Read(buffer, 0, buffer.Length);
+                        barcode.AppendFormat("{0}",
+                            Encoding.ASCII.GetString(buffer, 0, read));
+                    } while (stream.DataAvailable);
+                    
+                    Console.WriteLine($"Barcode: {barcode}");
+                }
+                else {
+                    Console.WriteLine("Stream unreadable.");
+                }
+
+                // For obvious reasons, don't use Thread.Sleep() in async code but instead use Task.Delay()
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+﻿# Setup
+
+## WSL
+
+Follow the [installation guide by Microsoft](https://docs.microsoft.com/en-us/windows/wsl/install-win10). Do __not__ update to WSL2 as this breaks USB device passthrough.
+
+Install the Alpine distro either through the [Microsoft store](https://www.microsoft.com/fr-be/p/alpine-wsl/9p804crf0395?rtc=1&activetab=pivot:overviewtab) or [the github repo](https://github.com/yuk7/AlpineWSL).
+
+### Alpine setup
+
+Install the necessary packages.
+```bash
+apk add ser2net nano
+```
+
+Run ser2net with a specific config line. It is important to use **-C** as CLI param instead of **-c**.
+
+/dev/**ttyS4** is the equivalent of the **COM4** port, so this needs to be verified against the device manager on the Windows host.
+```bash
+ser2net -C <port>:raw:0:/dev/<ttyS4>:9600
+```
+
+### Test
+
+```bash
+apk add socat
+socat socat -d -d - TCP4:127.0.0.1:port
+```
+
